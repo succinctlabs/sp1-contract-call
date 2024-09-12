1ff20c6e22fcf19806faaddee60e8e5e6e9eb702
Yuwen Zhang <yuwen01@gmail.com>
2024-09-11 20:18:09 -0700

    use jonathan's trait

--- crates/client-executor/src/lib.rs
+++ crates/client-executor/src/lib.rs
@@ -7,6 +7,7 @@ use reth_evm_ethereum::EthEvmConfig;
 use reth_primitives::Header;
 use revm::{db::CacheDB, Database, Evm, EvmBuilder};
 use revm_primitives::{Address, BlockEnv, CfgEnvWithHandlerCfg, SpecId, TxKind, U256};
+use rsp_client_executor::io::WitnessInput;
 use rsp_witness_db::WitnessDb;
 
 /// Input to a contract call.
@@ -31,9 +32,12 @@ pub struct ClientExecutor {
 
 impl ClientExecutor {
     /// Instantiates a new [`ClientExecutor`]
-    pub fn new(mut state_sketch: EVMStateSketch) -> eyre::Result<Self> {
+    pub fn new(state_sketch: EVMStateSketch) -> eyre::Result<Self> {
         // let header = state_sketch.header.clone();
-        Ok(Self { witness_db: state_sketch.witness_db().unwrap(), header: state_sketch.header })
+        Ok(Self {
+            witness_db: state_sketch.redirect_witness_db().unwrap(),
+            header: state_sketch.header,
+        })
     }
 
     /// Executes the smart contract call with the given [`ContractInput`] in SP1.
