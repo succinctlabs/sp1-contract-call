b16bc95bd4cf1dcfbb977ab62dca465b59834093
Yuwen Zhang <yuwen01@gmail.com>
2024-09-16 12:37:47 -0700

    added another integration test for multiplexer

--- crates/host-executor/src/test.rs
+++ crates/host-executor/src/test.rs
@@ -4,9 +4,10 @@ use alloy_rpc_types::BlockNumberOrTag;
 use alloy_sol_macro::sol;
 use alloy_sol_types::SolCall;
 use revm_primitives::U256;
-use sp1_cc_client_executor::{ClientExecutor, ContractInput};
+use sp1_cc_client_executor::{ClientExecutor, ContractInput, ContractPublicValues};
 use url::Url;
 use ERC20Basic::totalSupplyCall;
+use IOracleHelper::getRatesCall;
 
 use crate::HostExecutor;
 
@@ -17,14 +18,69 @@ sol! {
     }
 }
 
-/// Address of Uniswap V3 pool.
-const CONTRACT: Address = address!("dAC17F958D2ee523a2206206994597C13D831ec7");
+sol! {
+    /// Interface to the multiplexer contract. It gets the exchange rates of many tokens, including
+    /// apxEth, ankrEth, and pufEth.
+    interface IOracleHelper {
+        function getRates(address[] memory collaterals) external view returns (uint256[] memory);
+    }
+}
 
-/// Address of the caller.
-const CALLER: Address = address!("0000000000000000000000000000000000000000");
+/// Multiplexer collateral addresses
+const COLLATERALS: [Address; 12] = [
+    address!("E95A203B1a91a908F9B9CE46459d101078c2c3cb"),
+    address!("9Ba021B0a9b958B5E75cE9f6dff97C7eE52cb3E6"),
+    address!("Be9895146f7AF43049ca1c1AE358B0541Ea49704"),
+    address!("7f39C581F595B53c5cb19bD0b3f8dA6c935E2Ca0"),
+    address!("A35b1B31Ce002FBF2058D22F30f95D405200A15b"),
+    address!("D9A442856C234a39a81a089C06451EBAa4306a72"),
+    address!("ae78736Cd615f374D3085123A210448E74Fc6393"),
+    address!("A1290d69c65A6Fe4DF752f95823fae25cB99e5A7"),
+    address!("ac3E018457B222d93114458476f3E3416Abbe38F"),
+    address!("9D39A5DE30e57443BfF2A8307A4256c8797A3497"),
+    address!("f951E335afb289353dc249e82926178EaC7DEd78"),
+    address!("Cd5fE23C85820F7B72D0926FC9b05b43E359b7ee"),
+];
 
 #[tokio::test(flavor = "multi_thread")]
-async fn test_e2e() -> eyre::Result<()> {
+async fn test_multiplexer() -> eyre::Result<()> {
+    let get_rates_call = getRatesCall { collaterals: COLLATERALS.to_vec() };
+
+    let contract_input = ContractInput {
+        contract_address: address!("0A8c00EcFA0816F4f09289ac52Fcb88eA5337526"),
+        caller_address: Address::default(),
+        calldata: get_rates_call,
+    };
+
+    let public_values = test_e2e(contract_input).await?;
+
+    let rates = getRatesCall::abi_decode_returns(&public_values.contractOutput, true)?._0;
+
+    println!("rates: {:?}", rates);
+
+    Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn test_total_supply() -> eyre::Result<()> {
+    let total_supply_call = totalSupplyCall {};
+    let contract_input = ContractInput {
+        contract_address: address!("dAC17F958D2ee523a2206206994597C13D831ec7"),
+        caller_address: Address::default(),
+        calldata: total_supply_call,
+    };
+    let public_values = test_e2e(contract_input).await?;
+
+    let total_supply = totalSupplyCall::abi_decode_returns(&public_values.contractOutput, true)?._0;
+    assert_eq!(total_supply, U256::from(54981730120396390u128));
+
+    Ok(())
+}
+
+async fn test_e2e<C: SolCall + Clone>(
+    contract_input: ContractInput<C>,
+) -> eyre::Result<ContractPublicValues> {
+    // Load environment variables.
     dotenv::dotenv().ok();
 
     // Which block transactions are executed on.
@@ -37,14 +93,7 @@ async fn test_e2e() -> eyre::Result<()> {
     let provider = ReqwestProvider::new_http(Url::parse(&rpc_url)?);
     let mut host_executor = HostExecutor::new(provider.clone(), block_number).await?;
 
-    // Make the call to the slot0 function.
-    let total_supply_call = totalSupplyCall {};
-    let contract_input = ContractInput {
-        contract_address: CONTRACT,
-        caller_address: CALLER,
-        calldata: total_supply_call,
-    };
-    let _total_supply = host_executor.execute(contract_input.clone()).await?._0;
+    let _contract_output = host_executor.execute(contract_input.clone()).await?;
 
     // Now that we've executed all of the calls, get the `EVMStateSketch` from the host executor.
     let state_sketch = host_executor.finalize().await?;
@@ -53,14 +102,5 @@ async fn test_e2e() -> eyre::Result<()> {
 
     let public_values = client_executor.execute(contract_input)?;
 
-    // Read the output, and then calculate the uniswap exchange rate.
-    //
-    // Note that this output is read from values commited to in the program using
-    // `sp1_zkvm::io::commit`.
-    let total_supply = totalSupplyCall::abi_decode_returns(&public_values.contractOutput, true)?._0;
-
-    // println!("total_supply: {}", total_supply);
-    assert_eq!(total_supply, U256::from(54981730120396390u128));
-
-    Ok(())
+    Ok(public_values)
 }
