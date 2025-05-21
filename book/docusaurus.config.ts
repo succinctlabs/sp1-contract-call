4609e436e087e789503afa957a4d1f0b52b143f7
leruaa <aurelien.catinon@gmail.com>
2025-05-21 05:59:15 -0700

    docs: init the book

---
+++ book/docusaurus.config.ts
@@ -0,0 +1,66 @@
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+    title: "SP1 Contract Call Book",
+    tagline: "Powered by Succinct SP1",
+    favicon: "img/favicon.ico",
+
+    // Set the production url of your site here
+    url: "https://succinctlabs.github.io/",
+    baseUrl: "/contract-call",
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: "succinctlabs", // Usually your GitHub org/user name.
+    projectName: "sp1-contract-call", // Usually your repo name.
+
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: "en",
+        locales: ["en"],
+    },
+
+    presets: [
+        [
+            "classic",
+            {
+                docs: {
+                    routeBasePath: "/",
+                    sidebarPath: "./sidebars.ts",
+                },
+                blog: false,
+                theme: {},
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        // Replace with your project's social card
+        image: "img/docusaurus-social-card.jpg",
+        navbar: {
+            title: "SP1 Contract Call",
+            items: [
+                {
+                    href: "https://github.com/succinctlabs/sp1-contract-call",
+                    label: "GitHub",
+                    position: "right",
+                },
+            ],
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
