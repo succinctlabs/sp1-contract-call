b6cfcb7b2f0ee78b9c22186b8fec6e7c5af11963
destinyae <109734765+destinyae@users.noreply.github.com>
2025-05-21 23:29:24 +0100

    update lib.rs
    typo error

--- crates/client-executor/src/lib.rs
+++ crates/client-executor/src/lib.rs
@@ -228,7 +228,7 @@ impl<'a> ClientExecutor<'a> {
 
     /// Returns the decoded logs matching the provided `filter`.
     ///
-    /// To be avaliable in the client, the logs need to be prefetched in the host first.
+    /// To be available in the client, the logs need to be prefetched in the host first.
     pub fn get_logs<E: SolEvent>(&self, filter: Filter) -> Result<Vec<Log<E>>, ClientError> {
         let params = FilteredParams::new(Some(filter));
 
